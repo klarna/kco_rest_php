981573c9d2da8b3027b2149be2e4cf5de35ff39c
Aleksander Zinovyev <alexions@yandex.ru>
2018-08-07 17:32:24 +0200

    Fix ResponseValidator checking: Content-Type now can contain encoding.

--- src/Klarna/Rest/Transport/UserAgent.php
+++ src/Klarna/Rest/Transport/UserAgent.php
@@ -34,7 +34,7 @@ class UserAgent implements UserAgentInterface
     /**
      * Version of the SDK.
      */
-    const VERSION = '2.2.0';
+    const VERSION = '4.0.0';
 
     /**
      * Components of the user agent.
