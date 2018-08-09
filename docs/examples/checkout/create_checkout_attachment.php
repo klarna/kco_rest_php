a43df6639523da3f356331da2b9904814b3470ed
Aleksander Zinovyev <alexions@yandex.ru>
2018-08-09 14:56:10 +0200

    Docs: Update CheckoutAPI examples. Add Exception handling. Remove fetching when creating a new order.

--- docs/examples/checkout/create_checkout_attachment.php
+++ docs/examples/checkout/create_checkout_attachment.php
@@ -29,51 +29,57 @@ $emd = [
     ]
 ];
 
-$checkout = new Klarna\Rest\Checkout\Order($connector);
-$checkout->create([
-    "purchase_country" => "gb",
-    "purchase_currency" => "gbp",
-    "locale" => "en-gb",
-    "order_amount" => 10000,
-    "order_tax_amount" => 2000,
-    "order_lines" => [
-        [
-            "type" => "physical",
-            "reference" => "123050",
-            "name" => "Tomatoes",
-            "quantity" => 10,
-            "quantity_unit" => "kg",
-            "unit_price" => 600,
-            "tax_rate" => 2500,
-            "total_amount" => 6000,
-            "total_tax_amount" => 1200
+try {
+    $checkout = new Klarna\Rest\Checkout\Order($connector);
+    $checkout->create([
+        "purchase_country" => "gb",
+        "purchase_currency" => "gbp",
+        "locale" => "en-gb",
+        "order_amount" => 10000,
+        "order_tax_amount" => 2000,
+        "order_lines" => [
+            [
+                "type" => "physical",
+                "reference" => "123050",
+                "name" => "Tomatoes",
+                "quantity" => 10,
+                "quantity_unit" => "kg",
+                "unit_price" => 600,
+                "tax_rate" => 2500,
+                "total_amount" => 6000,
+                "total_tax_amount" => 1200
+            ],
+            [
+                "type" => "physical",
+                "reference" => "543670",
+                "name" => "Bananas",
+                "quantity" => 1,
+                "quantity_unit" => "bag",
+                "unit_price" => 5000,
+                "tax_rate" => 2500,
+                "total_amount" => 4000,
+                "total_discount_amount" => 1000,
+                "total_tax_amount" => 800
+            ]
         ],
-        [
-            "type" => "physical",
-            "reference" => "543670",
-            "name" => "Bananas",
-            "quantity" => 1,
-            "quantity_unit" => "bag",
-            "unit_price" => 5000,
-            "tax_rate" => 2500,
-            "total_amount" => 4000,
-            "total_discount_amount" => 1000,
-            "total_tax_amount" => 800
+        "merchant_urls" => [
+            "terms" => "http://www.merchant.com/toc",
+            "checkout" => "http://www.merchant.com/checkout?klarna_order_id={checkout.order.id}",
+            "confirmation" => "http://www.merchant.com/thank-you?klarna_order_id={checkout.order.id}",
+            "push" => "http://www.merchant.com/create_order?klarna_order_id={checkout.order.id}"
+        ],
+        "attachment" => [
+            "content_type" => "application/vnd.klarna.internal.emd-v2+json",
+            "body" => json_encode($emd)
         ]
-    ],
-    "merchant_urls" => [
-        "terms" => "http://www.merchant.com/toc",
-        "checkout" => "http://www.merchant.com/checkout?klarna_order_id={checkout.order.id}",
-        "confirmation" => "http://www.merchant.com/thank-you?klarna_order_id={checkout.order.id}",
-        "push" => "http://www.merchant.com/create_order?klarna_order_id={checkout.order.id}"
-    ],
-    "attachment" => [
-        "content_type" => "application/vnd.klarna.internal.emd-v2+json",
-        "body" => json_encode($emd)
-    ]
-]);
+    ]);
 
-$checkout->fetch();
+    // Store checkout order id
+    $orderId = $checkout->getId();
 
-// Store checkout order id
-$orderId = $checkout->getId();
+    // Get some data if needed
+    print_r($checkout['attachment']);
+
+} catch (Exception $e) {
+    echo 'Caught exception: ',  $e->getMessage(), "\n";
+}
--- docs/examples/checkout/fetch_checkout.php
+++ docs/examples/checkout/fetch_checkout.php
@@ -15,5 +15,17 @@ $connector = Klarna\Rest\Transport\Connector::create(
     Klarna\Rest\Transport\ConnectorInterface::EU_TEST_BASE_URL
 );
 
-$checkout = new Klarna\Rest\Checkout\Order($connector, $orderId);
-$checkout->fetch();
+try {
+    $checkout = new Klarna\Rest\Checkout\Order($connector, $orderId);
+    $checkout->fetch();
+
+    // Get some data if needed
+    echo <<<ORDER
+         OrderID: $checkout[order_id]
+    Order status: $checkout[status]
+    HTML snippet: $checkout[html_snippet]
+ORDER;
+
+} catch (Exception $e) {
+    echo 'Caught exception: ',  $e->getMessage(), "\n";
+}
