7c772b12f8654539390edcb75f19ff4126ea72b1
Aleksander Zinovyev <alexions@yandex.ru>
2018-08-13 10:16:04 +0200

    Add support of Payments API.

---
+++ src/Klarna/Rest/Payments/Orders.php
@@ -0,0 +1,132 @@
+<?php
+/**
+ * Copyright 2014 Klarna AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * File containing the Orders class.
+ */
+
+namespace Klarna\Rest\Payments;
+
+use GuzzleHttp\Exception\RequestException;
+use Klarna\Exception\NonApplicableException;
+use Klarna\Rest\Resource;
+use Klarna\Rest\Transport\Connector;
+use Klarna\Rest\Transport\Exception\ConnectorException;
+
+/**
+ * Payments order resource.
+ */
+class Orders extends Resource
+{
+    /**
+     * {@inheritDoc}
+     */
+    const ID_FIELD = 'authorization_token';
+
+    /**
+     * {@inheritDoc}
+     */
+    public static $path = '/payments/v1/authorizations';
+
+    /**
+     * Constructs an order instance.
+     *
+     * @param Connector $connector HTTP transport connector
+     * @param string    $authorizationToken   Authorization Token
+     */
+    public function __construct(Connector $connector, $authorizationToken)
+    {
+        parent::__construct($connector);
+
+        $this->setLocation(self::$path . "/{$authorizationToken}");
+        $this[static::ID_FIELD] = $authorizationToken;
+    }
+
+    /**
+     * Not applicable.
+     *
+     * @throws NonApplicableException
+     */
+    public function fetch()
+    {
+        throw NonApplicableException('Not applicable');
+    }
+
+    /**
+     * Creates the resource.
+     *
+     * @param array $data Creation data
+     *
+     * @throws ConnectorException When the API replies with an error response
+     * @throws RequestException   When an error is encountered
+     * @throws \RuntimeException  If the location header is missing
+     * @throws \RuntimeException  If the API replies with an unexpected response
+     * @throws \LogicException    When Guzzle cannot populate the response
+     *
+     * @return array Order data
+     */
+    public function create(array $data)
+    {
+        $data = $this->post(self::$path . '/order', $data)
+            ->status('200')
+            ->contentType('application/json');
+
+        return $data;
+    }
+
+    /**
+     * Cancels the authorization.
+     *
+     * @throws ConnectorException        When the API replies with an error response
+     * @throws RequestException          When an error is encountered
+     * @throws \RuntimeException         On an unexpected API response
+     * @throws \RuntimeException         If the response content type is not JSON
+     * @throws \InvalidArgumentException If the JSON cannot be parsed
+     * @throws \LogicException           When Guzzle cannot populate the response
+     *
+     * @return self
+     */
+    public function cancelAuthorization()
+    {
+        $this->delete($this->getLocation())
+            ->status('204')
+            ->contentType('application/json');
+
+        return $this;
+    }
+
+    /**
+     * Generates consumer token.
+     *
+     * @param array $data Token data
+     *
+     * @throws ConnectorException When the API replies with an error response
+     * @throws RequestException   When an error is encountered
+     * @throws \RuntimeException  If the location header is missing
+     * @throws \RuntimeException  If the API replies with an unexpected response
+     * @throws \LogicException    When Guzzle cannot populate the response
+     *
+     * @return array Token data
+     */
+    public function generateToken(array $data)
+    {
+        $data = $this->post(self::$path . '/customer-token', $data)
+            ->status('200')
+            ->contentType('application/json')
+            ->getJson();
+
+        return $data;
+    }
+}
