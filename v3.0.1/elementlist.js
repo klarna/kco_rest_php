9378f46a678abe0757099718d73e4eff92ae6cde
Aleksander Zinovyev <alexions@yandex.ru>
2018-11-13 17:48:33 +0100

    Add reference for all PHP SDK versions.

---
+++ v3.0.1/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","Klarna\\Rest\\Checkout\\Order"],["c","Klarna\\Rest\\OrderManagement\\Capture"],["c","Klarna\\Rest\\OrderManagement\\Order"],["c","Klarna\\Rest\\Resource"],["c","Klarna\\Rest\\Transport\\Connector"],["c","Klarna\\Rest\\Transport\\ConnectorInterface"],["c","Klarna\\Rest\\Transport\\Exception\\ConnectorException"],["c","Klarna\\Rest\\Transport\\ResponseValidator"],["c","Klarna\\Rest\\Transport\\UserAgent"],["c","Klarna\\Rest\\Transport\\UserAgentInterface"]];
