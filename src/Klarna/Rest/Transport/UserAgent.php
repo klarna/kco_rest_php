f535ed94dffb79f01d271935eccabefee6f3c168
Aleksander Zinovyev <alexions@yandex.ru>
2019-01-23 15:21:40 +0100

    Bump the SDK version.

--- src/Klarna/Rest/Transport/UserAgent.php
+++ src/Klarna/Rest/Transport/UserAgent.php
@@ -34,7 +34,7 @@ class UserAgent implements UserAgentInterface
     /**
      * Version of the SDK.
      */
-    const VERSION = '4.1.2';
+    const VERSION = '4.1.3';
 
     /**
      * Components of the user agent.
