a99604bfce6e7acfe150d756d03d751b47292d8d
Alexander Zinovev <alexions@yandex.ru>
2019-06-07 15:49:52 +0200

    Fix SPL Autoloader in order to follow the PSR standards.

--- src/autoload.php
+++ src/autoload.php
@@ -17,14 +17,14 @@
  * File containing the abstract base resource class.
  */
 
-define('BASE_DIR', __DIR__);
-
 spl_autoload_register(function ($class_path) {
+    $baseDir = __DIR__;
+
     $parts = explode('\\', $class_path);
     $class_name = array_pop($parts) . '.php';
     $path = implode(DIRECTORY_SEPARATOR, $parts);
     $path .= DIRECTORY_SEPARATOR . $class_name;
-    if (file_exists(BASE_DIR . DIRECTORY_SEPARATOR . $path)) {
-        require_once BASE_DIR . DIRECTORY_SEPARATOR . $path;
+    if (file_exists($baseDir . DIRECTORY_SEPARATOR . $path)) {
+        require_once $baseDir . DIRECTORY_SEPARATOR . $path;
     }
 });
